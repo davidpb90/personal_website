ffd05a156ea7116ec9d1e3f251e8c8177a094858
David Pardo <davidpb90@gmail.com>
2023-07-16 01:21:43 +0200

    modified toggle mode

--- js/color-modes.js
+++ js/color-modes.js
@@ -1,8 +1,10 @@
-document.getElementById('btnSwitch').addEventListener('click',()=>{
-    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme','light')
-    }
-    else {
-        document.documentElement.setAttribute('data-bs-theme','dark')
-    }
-})
+var button = {
+  document.getElementById('btnSwitch').addEventListener('click',()=>{
+      if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+          document.documentElement.setAttribute('data-bs-theme','light')
+      }
+      else {
+          document.documentElement.setAttribute('data-bs-theme','dark')
+      }
+  })
+}
