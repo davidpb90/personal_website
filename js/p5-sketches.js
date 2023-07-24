a0d3976bb838ad8324f0d19874c107e9d865cc77
David Pardo <davidpb90@gmail.com>
2023-07-24 21:00:24 +0200

    added diffusion animation and fixed issue with mobile phones

--- js/p5-sketches.js
+++ js/p5-sketches.js
@@ -5,6 +5,15 @@ function setup() {
 function draw() {
   clear();
   // background(255);
-  fill(random(255),random(255),random(255));
+  fill(0);
   ellipse(mouseX, mouseY, 100, 100);
+  if (mouseIsPressed) {
+    var radius = 100;
+    var alpha = 255;
+    for (var i = 0; i < radius; i++) {
+      fill(alpha);
+      ellipse(mouseX, mouseY, i, i);
+      alpha -= 5;
+    }
+  }
 }
